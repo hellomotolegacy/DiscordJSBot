b1f69ca75a15bf8baa83f5294b81b28fb2c4de8d
unknown <hellomotolegacy@gmail.com>
2018-01-21 03:14:06 -0500

    Moved message parsing to separate function

--- bot.js
+++ bot.js
@@ -2,13 +2,18 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+function CheckCommands(msg, com) {
+	switch(com) {
+		case 'ping':
+			msg.reply('Pong!');
+	}
+}
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+	CheckCommands(msg, msg.content); //cleaner to make one function for parsing sent messages.
 });
 
 client.login('NDA0NDQ0MjA5MzQ5NjU2NTg3.DUV7kA.A1JXjp49UYA4cbixC3wm6t1QYfU');
