6f8c39f20ccd180700f5070c9367f2489a524a2f
unknown <hellomotolegacy@gmail.com>
2018-01-21 12:39:38 -0500

    more reformatting

--- bot.js
+++ bot.js
@@ -10,18 +10,23 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!\n\n\n`);
 	client.user.setActivity(`& thonking`);
+	client.user.setStatus('dnd');
 });
 
 client.on("message", async message => {
+	var cmd = message + ''; //message is an object, so make it a string.
+
+	if (!cmd.includes(prefix))
+		return; //message doesn't include prefix, so ignore it.
+
+	var com = cmd.split(prefix)[1]; //remove prefix
+
     if (message.author.bot)
 		return; //don't parse messages sent by other bots (including ourself)
 
-	const args = message.content.slice('--').trim().split(/ +/g); //get arguments to a command
-	const command = args.shift().toLowerCase(); //make everything lower case.
-
-	if(command === "--ping") {
+	if(com === "ping") {
     	const m = await message.channel.send("Ping?"); //send a message while calculating ping
     	m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`); //edit after calculated
 	}
