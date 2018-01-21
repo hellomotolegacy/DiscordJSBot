fc8c04cf066294a4f59bcaae0fb86176c78cb20f
unknown <hellomotolegacy@gmail.com>
2018-01-21 11:57:32 -0500

    Reformatted (again), added status & ping command

--- bot.js
+++ bot.js
@@ -1,19 +1,30 @@
+/*
+   ========================
+            BOT.JS
+    AUTHOR(S): IAN BOWLING
+   ========================
+*/
+
+const prefix = '--'; //easy to change prefix
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+	client.user.setActivity(`& thonking`);
 });
 
-function CheckCommands(msg, com) {
-	switch(com) {
-		case 'ping':
-			msg.reply('Pong!');
-	}
-}
+client.on("message", async message => {
+    if (message.author.bot)
+		return; //don't parse messages sent by other bots (including ourself)
 
-client.on('message', msg => {
-	CheckCommands(msg, msg.content); //cleaner to make one function for parsing sent messages.
+	const args = message.content.slice('--').trim().split(/ +/g); //get arguments to a command
+	const command = args.shift().toLowerCase(); //make everything lower case.
+
+	if(command === "--ping") {
+    	const m = await message.channel.send("Ping?"); //send a message while calculating ping
+    	m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`); //edit after calculated
+	}
 });
 
 client.login('NDA0NDQ0MjA5MzQ5NjU2NTg3.DUV7kA.A1JXjp49UYA4cbixC3wm6t1QYfU');
